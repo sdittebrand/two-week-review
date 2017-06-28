b6fb0ed1b7c3a3d5ab65ec7571beb787bd0b954e
Seth Dittebrand <sdittebrand@gmail.com>
2017-06-28 10:37:11 -0400

    review

---
+++ review.rb
@@ -0,0 +1,19 @@
+def guessinggame()
+    n = rand 10
+    puts "Guess a number between 0 and 9"
+
+    loop do
+        guess = gets.chomp.to_i
+
+        if guess = n
+            puts "You got it right!"
+            break
+        elsif guess > n 
+            puts "Too high, try again!"
+        elsif guess < n 
+            puts "Too low, try again!"
+        end
+    end
+end
+
+guessinggame()
